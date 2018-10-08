fbe244984dcd0b166a7c6a60bc229a6f43f4b1dc
Anton Lazarev <antonok35@gmail.com>
2018-10-07 22:50:58 -0400

    spacing

--- src/server/app.js
+++ src/server/app.js
@@ -22,74 +22,74 @@ const operations = ['backup', 'write', 'make', 'flash', 'clean'];
 
 // Shell commands corresponding to server operations
 const ext_commands = {
-	backup: "cp " + path.join(layermaps_dir, 'layermaps.c') +
-		" " + path.join(layermaps_dir, 'layermaps.c.bak'),
-	write: "mv " + path.join('/tmp', 'layermaps.c') +
-		" " + path.join(layermaps_dir, 'layermaps.c'),
-	make: "make -C " + firmware_dir,
-	flash: "teensy_loader_cli --mcu=atmega32u4 -w "
-		+ path.join(firmware_dir, 'firmware.hex'),
-	clean: "make -C " + firmware_dir + " clean"
+    backup: "cp " + path.join(layermaps_dir, 'layermaps.c') +
+        " " + path.join(layermaps_dir, 'layermaps.c.bak'),
+    write: "mv " + path.join('/tmp', 'layermaps.c') +
+        " " + path.join(layermaps_dir, 'layermaps.c'),
+    make: "make -C " + firmware_dir,
+    flash: "teensy_loader_cli --mcu=atmega32u4 -w "
+        + path.join(firmware_dir, 'firmware.hex'),
+    clean: "make -C " + firmware_dir + " clean"
 };
 
 // Setup socket connection
 io.on('connection', socket => {
-	console.log('Client connected.');
-	// Read layermaps.c file directly from firmware directory and send it
-	fs.readFile(path.join(layermaps_dir, 'layermaps.c'),
-		'ascii',
-		(err, layermaps_text) => {
-			if(err) {
-				console.log("Could not read layermaps.c!");
-				console.log("Exiting.");
-				process.exit()
-			}
-			socket.emit('layermaps.c', layermaps_text);
-		});
-	// Process operation requests from the client
-	socket.on('layermaps.c', async function(data) {
-		let options = data.options;
-		let file_text = data.file_text;
-		await writeTmpFile('layermaps.c', file_text);
-		//for(i in operations) {
-			//op = operations[i];
-		for(op in ext_commands) {
-			if(options[op]) {
-				let res = await do_op(op);
-				if(res === -1) {
-					return;
-				}
-			}
-		}
-		socket.emit('progress',
-			{ op: "done", during: "", traceback: "" });
-	});
+    console.log('Client connected.');
+    // Read layermaps.c file directly from firmware directory and send it
+    fs.readFile(path.join(layermaps_dir, 'layermaps.c'),
+        'ascii',
+        (err, layermaps_text) => {
+            if(err) {
+                console.log("Could not read layermaps.c!");
+                console.log("Exiting.");
+                process.exit()
+            }
+            socket.emit('layermaps.c', layermaps_text);
+        });
+    // Process operation requests from the client
+    socket.on('layermaps.c', async function(data) {
+        let options = data.options;
+        let file_text = data.file_text;
+        await writeTmpFile('layermaps.c', file_text);
+        //for(i in operations) {
+            //op = operations[i];
+        for(op in ext_commands) {
+            if(options[op]) {
+                let res = await do_op(op);
+                if(res === -1) {
+                    return;
+                }
+            }
+        }
+        socket.emit('progress',
+            { op: "done", during: "", traceback: "" });
+    });
 
-	async function do_op(op) {
-		socket.emit('progress',
-			{ op: op, during: "", traceback: "" });
-		let exitcode = await exec(ext_commands[op]).catch(err => {
-			console.log("The following error occurred during '" +
-				op + "':");
-			console.log(err);
-			socket.emit('progress',
-				{ op: "error", during: op, traceback: err });
-			return -1;
-		});
-		return exitcode;
-	}
+    async function do_op(op) {
+        socket.emit('progress',
+            { op: op, during: "", traceback: "" });
+        let exitcode = await exec(ext_commands[op]).catch(err => {
+            console.log("The following error occurred during '" +
+                op + "':");
+            console.log(err);
+            socket.emit('progress',
+                { op: "error", during: op, traceback: err });
+            return -1;
+        });
+        return exitcode;
+    }
 });
 
 // Write text to a file in the firmware directory
 async function writeTmpFile(name, contents) {
-	fs.writeFileSync(path.join('/tmp', name), contents, err => {
-		return console.log(err);
-	});
-	console.log("Wrote " + name);
+    fs.writeFileSync(path.join('/tmp', name), contents, err => {
+        return console.log(err);
+    });
+    console.log("Wrote " + name);
 }
 
 http.listen(PORT, () =>
-	console.log('kb layout manager backend running on http://localhost:'
-		+ PORT));
+    console.log('kb layout manager backend running on http://localhost:'
+        + PORT));
 
 app.use(express.static(path.join('dist')));
--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,29 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const { VueLoaderPlugin } = require("vue-loader");
 
 module.exports = {
-	mode: 'development',
-	entry: {
-		main: './src/client/client.js',
-	},
-	output: {
-		filename: 'client.js',
-	},
-	devtool: 'source-map',
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				//exclude: /(node_modules|dist|app.js)/,
-				use: ['babel-loader', 'eslint-loader'],
-			},
-			{
-				test: /\.s?(c|a)?ss$/,
-				loaders: ['style-loader', 'css-loader', 'sass-loader'],
-			},
-			{
-				test: /\.html$/,
-				use: ['html-loader'],
-			},
+    mode: 'development',
+    entry: {
+        main: './src/client/client.js',
+    },
+    output: {
+        filename: 'client.js',
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                //exclude: /(node_modules|dist|app.js)/,
+                use: ['babel-loader', 'eslint-loader'],
+            },
+            {
+                test: /\.s?(c|a)?ss$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader'],
+            },
             {
                 test: /\.vue$/,
                 use: ['vue-loader'],
@@ -33,13 +33,13 @@ module.exports = {
                 test: /\.ico$/,
                 loader: 'file-loader?name=[name].[ext]',
             },
-		],
-	},
-	plugins: [
-		new HtmlWebPackPlugin({
-			template: "src/client/index.html",
-			filename: "index.html",
-		}),
+        ],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "src/client/index.html",
+            filename: "index.html",
+        }),
         new VueLoaderPlugin(),
-	],
+    ],
 };
